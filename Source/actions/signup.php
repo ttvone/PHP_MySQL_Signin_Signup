1f5151dc3434f1fc28b370847dfba108276eb633
Wanchaloem Laoket <ttvone@hotmail.com>
2020-01-26 01:33:10 +0700

    EP-8

--- Source/actions/signup.php
+++ Source/actions/signup.php
@@ -1,9 +1,32 @@
 <?php
 include_once '../configs/database.php';
 
-$result = $mysqli->query('SELECT * FROM users');
-echo '<pre>';
-print_r($result->fetch_all(MYSQLI_ASSOC));
-echo '</pre>';
+if(isset($_POST['name']) && isset($_POST['username']) && isset($_POST['password'])) 
+{
+    $name       = trim($_POST['name']);
+    $username   = trim($_POST['username']);
+    $password   = password_hash(trim($_POST['password']), PASSWORD_DEFAULT);
 
-// print_r($_POST);
+    $sql        = 'INSERT INTO users (user_name, user_username, user_password) VALUES(?, ?, ?)';
+    $stmt       = $mysqli->prepare($sql);
+    $stmt->bind_param('sss', $name, $username, $password);
+    
+    if($stmt->execute()) 
+    {
+        header('Location: /signin.php');
+    } 
+    else 
+    {
+        echo '<script>';
+        echo 'alert("' . $stmt->error . '");';
+        echo 'history.back();';
+        echo '</script>';
+    }
+}
+else 
+{
+    echo '<script>';
+    echo 'alert("Error not found name or username or password");';
+    echo 'history.back();';
+    echo '</script>';
+}
