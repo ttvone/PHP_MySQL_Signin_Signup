588814e6bc2d4c14030eb00aadd42751ff7534df
Wanchaloem Laoket <ttvone@hotmail.com>
2020-01-25 18:26:34 +0700

    Add signup

---
+++ Source/signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Signup page</title>
+</head>
+
+<body>
+    <form action="">
+        <div>
+            <input type="text" placeholder="ชื่อและนามสกุล">
+        </div>
+        <div>
+            <input type="text" placeholder="ชื่อผู้ใช้งาน">
+        </div>
+        <div>
+            <input type="text" placeholder="รหัสผ่าน">
+        </div>
+        <div>
+            <input type="text" placeholder="ยืนยันรหัสผ่าน">
+        </div>
+        <div>
+            <input type="submit" value="สมัครสมาชิก">
+        </div>
+        <div>
+            มีบัญชีอยู่แล้วใช่หรือไม่?
+            <a href="">เข้าสู่ระบบ</a>
+        </div>
+    </form>
+</body>
+
+</html>
